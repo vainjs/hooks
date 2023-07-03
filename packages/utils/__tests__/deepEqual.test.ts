d73db2e05792e10d06fe4ad7e5ed08f35c4dfb8d
youngluo <youngluooo@gmail.com>
2023-07-03 23:30:50 +0800

    test: add deepEqual test case

---
+++ packages/utils/__tests__/deepEqual.test.ts
@@ -0,0 +1,43 @@
+import deepEqual from '../deepEqual'
+
+describe('deepEqual', () => {
+  it('deepEqual should work with object', async () => {
+    const target = { a: 1 }
+    expect(deepEqual(target, target)).toBe(true)
+    expect(deepEqual({ a: 1, b: 2 }, { b: 2, a: 1 })).toBe(true)
+    expect(deepEqual({ a: 1, b: 2 }, { a: 1 })).toBe(false)
+    expect(
+      deepEqual(
+        { a: 1, b: 2, c: [1, 2, { a: 1 }] },
+        { a: 1, b: 2, c: [1, 2, { a: 1 }] }
+      )
+    ).toBe(true)
+  })
+
+  it('deepEqual should work with array', async () => {
+    const target = [1, 2, 3]
+    expect(deepEqual(target, target)).toBe(true)
+    expect(deepEqual([1, 2, 3], [1, 3, 2])).toBe(false)
+    expect(deepEqual([1, 2, 3], [1, 2, 3])).toBe(true)
+  })
+
+  it('deepEqual should work with number', async () => {
+    expect(deepEqual(1, 1)).toBe(true)
+    expect(deepEqual(1, 2)).toBe(false)
+    expect(deepEqual(1, new Number(1))).toBe(false)
+    expect(deepEqual(1, new Number('1'))).toBe(false)
+    expect(deepEqual(new Number('1'), new Number('1'))).toBe(true)
+  })
+
+  it('deepEqual should work with boolean', async () => {
+    expect(deepEqual(true, false)).toBe(false)
+    expect(deepEqual(true, new Boolean(true))).toBe(false)
+    expect(deepEqual(new Boolean(true), new Boolean(true))).toBe(true)
+  })
+
+  it('deepEqual should work with string', async () => {
+    expect(deepEqual('1', '1')).toBe(true)
+    expect(deepEqual('1', new String('1'))).toBe(false)
+    expect(deepEqual(new String('1'), new String('1'))).toBe(true)
+  })
+})
