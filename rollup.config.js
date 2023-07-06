5e860c44829cb5b1aff07a1926bc858586c22b97
youngluo <youngluooo@gmail.com>
2023-07-06 23:20:06 +0800

    build: update

--- rollup.config.js
+++ rollup.config.js
@@ -1,69 +1,75 @@
 import typescript from '@rollup/plugin-typescript'
-import clear from 'rollup-plugin-clear'
-import pkg from './package.json' assert { type: 'json' }
-import { globSync } from 'glob'
 import { fileURLToPath } from 'node:url'
+import clear from 'rollup-plugin-clear'
+import { globSync } from 'glob'
+import path from 'node:path'
+import pkg from './package.json' assert { type: 'json' }
+
+const getDir = (url) => path.parse(url).dir
+
+const cjsDir = getDir(pkg.main)
+const esDir = getDir(pkg.module)
+const entries = Object.fromEntries(
+  globSync('packages/**/*.ts', { ignore: '**/__tests__/**' }).map((file) => [
+    path.relative(
+      'packages',
+      file.slice(0, file.length - path.extname(file).length)
+    ),
+    fileURLToPath(new URL(file, import.meta.url)),
+  ])
+)
 
 export default [
   {
-    input: Object.fromEntries(
-      globSync('packages/**/*.ts').map((file) => [
-        console.log(file),
-        // 这里将删除 `src/` 以及每个文件的扩展名。
-        // 因此，例如 src/nested/foo.js 会变成 nested/foo
-        // path.relative(
-        //   'src',
-        //   file.slice(0, file.length - path.extname(file).length)
-        // ),
-        // 这里可以将相对路径扩展为绝对路径，例如
-        // src/nested/foo 会变成 /project/src/nested/foo.js
-        fileURLToPath(new URL(file, import.meta.url)),
-      ])
-    ),
+    input: entries,
     output: [
       {
-        file: pkg.main,
-        format: 'cjs',
-      },
-      {
-        file: pkg.module,
         format: 'es',
+        dir: esDir,
+        plugins: [
+          clear({
+            targets: ['dist'],
+          }),
+          typescript({ compilerOptions: { declarationDir: esDir } }),
+        ],
+      },
+    ],
+    external: Object.keys(pkg.peerDependencies),
+  },
+  {
+    input: entries,
+    output: [
+      {
+        format: 'cjs',
+        dir: cjsDir,
       },
     ],
     plugins: [
       clear({
         targets: ['dist'],
       }),
-      typescript(),
+      typescript({ compilerOptions: { declarationDir: cjsDir } }),
     ],
     external: Object.keys(pkg.peerDependencies),
   },
-  // {
-  //   input: 'packages/index.ts',
-  //   output: [
-  //     {
-  //       file: pkg.main,
-  //       format: 'cjs',
-  //     },
-  //     {
-  //       file: pkg.module,
-  //       format: 'es',
-  //     },
-  //     {
-  //       file: pkg.unpkg,
-  //       format: 'umd',
-  //       name: 'ihooks',
-  //       globals: {
-  //         react: 'React',
-  //       },
-  //     },
-  //   ],
-  //   external: Object.keys(pkg.peerDependencies),
-  //   plugins: [
-  //     clear({
-  //       targets: ['dist'],
-  //     }),
-  //     typescript(),
-  //   ],
-  // },
+  {
+    input: 'packages/index.ts',
+    output: [
+      {
+        file: pkg.unpkg,
+        format: 'umd',
+        name: 'ihooks',
+        globals: {
+          react: 'React',
+        },
+      },
+    ],
+    external: Object.keys(pkg.peerDependencies),
+    plugins: [
+      clear({
+        targets: ['dist'],
+      }),
+      typescript({ compilerOptions: { declaration: false } }),
+    ],
+  },
 ]
